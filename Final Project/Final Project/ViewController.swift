f2f43af5e74562571091deaf90511ef37ebde06c
joe135730 <a0953868706@gmail.com>
2024-10-16 14:34:23 -0400

    navigationController setting

--- Final Project/Final Project/ViewController.swift
+++ Final Project/Final Project/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let loginView = LoginView()
+
+    override func loadView() {
+        view = loginView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // type something
+        title = "Login"
+        
+        loginView.loginButton.addTarget(self, action: #selector(onAddBarButtonTapped), for: .touchUpInside)
+
     }
 
+    @objc func onAddBarButtonTapped(){
+        //MARK: Put your own controller here to navigate to test
+//        let addContactController = AddContactController()
+//        addContactController.delegate = self
+//        navigationController?.pushViewController(addContactController, animated: true)
+        
+        
+    }
 
 }
 
