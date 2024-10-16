cdab8de7425a4e56c4ce1480b115ce9eeb12b912
joe135730 <a0953868706@gmail.com>
2024-10-16 13:31:05 -0400

    First Commit

---
+++ Final Project/Final Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final Project
+//
+//  Created by Che-Yi Wu on 2024/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
