768b076a3200310d5aae4a788a50024315e2a9aa
yuhsuan <buybtsticket@gmail.com>
2024-11-26 18:39:43 -0500

    storage

--- Final Project/Final Project/Register Screen/RegisterFirebaseManager.swift
+++ Final Project/Final Project/Register Screen/RegisterFirebaseManager.swift
@@ -87,5 +87,60 @@ extension RegisterScreenController{
             print("Error encoding friend data: \(error.localizedDescription)")
         }
     }
+    
+    //MARK: storage
+    func uploadProfilePhotoToStorage(){
+            var profilePhotoURL:URL?
+            
+            //MARK: Upload the profile photo if there is any...
+            if let image = pickedImage{
+                if let jpegData = image.jpegData(compressionQuality: 80){
+                    let storageRef = storage.reference()
+                    let imagesRepo = storageRef.child("imagesUsers")
+                    let imageRef = imagesRepo.child("\(NSUUID().uuidString).jpg")
+                    
+                    let uploadTask = imageRef.putData(jpegData, completion: {(metadata, error) in
+                        if error == nil{
+                            imageRef.downloadURL(completion: {(url, error) in
+                                if error == nil{
+                                    profilePhotoURL = url
+                                    self.registerUser(photoURL: profilePhotoURL)
+                                }
+                            })
+                        }
+                    })
+                }
+            }else{
+                registerUser(photoURL: profilePhotoURL)
+            }
+        }
+        
+        func registerUser(photoURL: URL?){
+            if let name = registerView.textFieldName.text,
+               let email = registerView.textFieldEmail.text,
+               let password = registerView.textFieldPassword.text{
+                Auth.auth().createUser(withEmail: email, password: password, completion: {result, error in
+                    if error == nil{
+                        self.setNameAndPhotoOfTheUserInFirebaseAuth(name: name, email: email, photoURL: photoURL)
+                    }
+                })
+            }
+        }
+        
+        func setNameAndPhotoOfTheUserInFirebaseAuth(name: String, email: String, photoURL: URL?){
+            let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+            changeRequest?.displayName = name
+            changeRequest?.photoURL = photoURL
+            
+            print("\(photoURL)")
+            changeRequest?.commitChanges(completion: {(error) in
+                if error != nil{
+                    print("Error occured: \(String(describing: error))")
+                }else{
+                    self.hideActivityIndicator()
+                    self.navigationController?.popViewController(animated: true)
+                }
+            })
+        }
 
 }
