db0326b8bf9150467dee440982de2b5f1eba4eb2
joe135730 <a0953868706@gmail.com>
2024-11-26 18:27:14 -0500

    Revert "Merge branch 'mainScreen'"
    This reverts commit 7237aea1b3b4d809ca94bed5c01e01804e010031, reversing
    changes made to c87e36e9e5cb5474908b608af38a7f3528b747ac.

--- Final Project/Final Project/AppDelegate.swift
+++ Final Project/Final Project/AppDelegate.swift
@@ -11,9 +11,7 @@ import Firebase
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-        return .landscape
-    }
+
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
